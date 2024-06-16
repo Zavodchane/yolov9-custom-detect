1bedf68bf205e366ab5d0c97c727d78e8d7fad70
Ivanov Leonid <39859999+LIvanoff@users.noreply.github.com>
2024-06-16 07:43:26 +0300

    Update custom_detect.py

--- custom_detect.py
+++ custom_detect.py
@@ -189,7 +189,7 @@ def run(
     # Print results
     csv_path = os.path.join(save_dir, "submission.csv")
     df = pd.DataFrame(predict)
-    df.to_csv(csv_path, index=False)
+    df.to_csv(csv_path, index=False, sep=';')
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
     if save_txt or save_img:
